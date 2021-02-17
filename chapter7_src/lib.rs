f79ca4c636997b039dad32b0c9fb6bcce6b5a6ce
Alexandru Vlăduţu <github@social.alessioalex.com>
2021-02-17 16:53:16 +0200

    chapter 7

---
+++ chapter7_src/lib.rs
@@ -0,0 +1,82 @@
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+/*
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it;
+    // we're not allowed to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+*/
+
+mod front_of_house;
+
+// use self::front_of_house::hosting;
+// re-exporting below
+// pub use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+/*
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {
+    // --snip--
+}
+
+fn function2() -> IoResult<()> {
+    // --snip--
+}
+*/
+
+// nested paths
+// use std::{cmp::Ordering, io};
+use std::io::{self, Write};
+// equivalent to:
+// use std::io;
+// use std::io::Write;
+
+// bring all public items defined in a path into scope with the glob operator
+use std::collections::*;
