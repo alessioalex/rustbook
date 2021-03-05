7fcdcc24294bcdea0c7c014354eed1c1753300f3
Alexandru Vlăduţu <github@social.alessioalex.com>
2021-03-05 17:23:06 +0200

    generics && traits (wip)

---
+++ src/10.1_generics_2.rs
@@ -0,0 +1,18 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+
+    println!("point.x = {}", integer_and_float.x());
+}
